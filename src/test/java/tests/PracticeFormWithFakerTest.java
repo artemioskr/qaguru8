f6627d18fe10faa01c1d42244bbbbadfbbae6f90
Artemiy Skryabin <artemio.skr@gmail.com>
2021-03-02 18:16:47 +0300

    add steps

--- src/test/java/tests/PracticeFormWithFakerTest.java
+++ src/test/java/tests/PracticeFormWithFakerTest.java
@@ -10,6 +10,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byValue;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 
 public class PracticeFormWithFakerTest extends TestBase {
@@ -19,54 +20,62 @@ public class PracticeFormWithFakerTest extends TestBase {
         Faker faker = new Faker();
 
         String firstName = faker.name().firstName();
-        String lastName  = faker.name().lastName();
-        String email     = faker.internet().emailAddress();
-        String gender    = "Male";
-        String phone     = faker.phoneNumber().subscriberNumber(10);
-        String subject   = "English";
+        String lastName = faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String gender = "Male";
+        String phone = faker.phoneNumber().subscriberNumber(10);
+        String subject = "English";
         String hobbies = "Sports";
-        String address   = faker.address().fullAddress();
-        String state     = "NCR";
-        String city      = "Delhi";
+        String address = faker.address().fullAddress();
+        String state = "NCR";
+        String city = "Delhi";
 
-        open("https://demoqa.com/automation-practice-form");
-        $(".main-header").shouldHave(text("Practice Form"));
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        });
 
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $(byText(gender)).click();
-        $("#userNumber").setValue(phone);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").$(byValue("1994")).click();
-        $(".react-datepicker__month-select").$(byValue("0")).click();
-        $(".react-datepicker__month").$(byText("5")).click();
-        $("#subjectsInput").setValue(subject).pressEnter();
-        $(byText(hobbies)).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/frog.jpg"));
-        $("#currentAddress").setValue(address).scrollTo();
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText(state)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(city)).click();
-        $("#submit").click();
+        step("Fill students registration form", () -> {
+            $("#firstName").setValue(firstName);
+            $("#lastName").setValue(lastName);
+            $("#userEmail").setValue(email);
+            $(byText(gender)).click();
+            $("#userNumber").setValue(phone);
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__year-select").$(byValue("1994")).click();
+            $(".react-datepicker__month-select").$(byValue("0")).click();
+            $(".react-datepicker__month").$(byText("5")).click();
+            $("#subjectsInput").setValue(subject).pressEnter();
+            $(byText(hobbies)).click();
+            $("#uploadPicture").uploadFile(new File("src/test/resources/frog.jpg"));
+            $("#currentAddress").setValue(address).scrollTo();
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText(state)).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText(city)).click();
+            $("#submit").click();
+        });
 
-        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(firstName + " " + lastName));
-        $(".table-responsive").shouldHave(text(email));
-        $(".table-responsive").shouldHave(text(gender));
-        $(".table-responsive").shouldHave(text(phone));
-        $(".table-responsive").shouldHave(text("05 January,1994"));
-        $(".table-responsive").shouldHave(text(subject));
-        $(".table-responsive").shouldHave(text(hobbies));
-        $(".table-responsive").shouldHave(text("frog.jpg"));
-        $(".table-responsive").shouldHave(text(address));
-        $(".table-responsive").shouldHave(text(state + " " + city));
+        step("Verify form", () -> {
+            $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text(firstName + " " + lastName));
+            $(".table-responsive").shouldHave(text(email));
+            $(".table-responsive").shouldHave(text(gender));
+            $(".table-responsive").shouldHave(text(phone));
+            $(".table-responsive").shouldHave(text("05 January,1994"));
+            $(".table-responsive").shouldHave(text(subject));
+            $(".table-responsive").shouldHave(text(hobbies));
+            $(".table-responsive").shouldHave(text("frog.jpg"));
+            $(".table-responsive").shouldHave(text(address));
+            $(".table-responsive").shouldHave(text(state + " " + city));
+        });
     }
 
     @Test
     public void negativeTest() {
-        open("https://demoqa.com/automation-practice-form");
-        $(".main-header").shouldHave(text("Pract234ice Form"));
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+            $(".main-header").shouldHave(text("Pract234ice Form"));
+        });
     }
 }
